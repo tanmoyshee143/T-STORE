92920d1374030c88ed00e929533fdbad69c3ed94
Tanmoy Shee <tanmoyshee143@gmail.com>
2023-02-21 16:37:04 +0530

    folder structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Router>
+      <div>Welcome to Tanmoy React E-Commerce Website</div>
+    </Router>
+  );
+};
+
+export default App;
